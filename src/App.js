681b90f8ae4be7aec985f27e4aa87a5bc74c9e46
Saunafo <janus@miriamjanus.com>
2025-04-04 00:13:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App with booking slots per sauna
 import React, { useState } from "react";
 import { Button } from "./components/ui/button";
-import { Card, CardContent } from "../components/ui/card";
+import { Card, CardContent } from "./components/ui/card";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
 import { Calendar } from "./components/ui/calendar";
